6c3633f370c0d8d2bbd218b2ba52800015240dc2
gmmunis <gmmunis@hotmail.com>
2024-02-28 16:23:55 -0300

    queries with knex

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,15 @@ import { knex } from './database'
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
 
-  return tables
+  return transaction
 })
 
 app
