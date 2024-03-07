ff523b51806813f1d96f735214065220d0f9bdaf
gmmunis <gmmunis@hotmail.com>
2024-03-07 14:59:51 -0300

    organizing the tests

--- test/createTransactions.spec.ts
+++ test/createTransactions.spec.ts
@@ -1,22 +1,24 @@
-import { afterAll, beforeAll, test } from 'vitest'
+import { afterAll, beforeAll, test, describe } from 'vitest'
 import { app } from '../src/app'
 import request from 'supertest'
 
-beforeAll(async () => {
-  await app.ready()
-})
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
 
-afterAll(async () => {
-  await app.close()
-})
+  afterAll(async () => {
+    await app.close()
+  })
 
-test('user can create a new transaction', async () => {
-  await request(app.server)
-    .post('/transactions')
-    .send({
-      title: 'New transaction',
-      amount: 5000,
-      type: 'credit',
-    })
-    .expect(201)
+  test('user can create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
 })
