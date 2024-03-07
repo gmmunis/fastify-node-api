9802455548cf98f97d008ce641ff5e4357727d7d
gmmunis <gmmunis@hotmail.com>
2024-03-07 14:50:47 -0300

    create transactions tests

---
+++ test/createTransactions.spec.ts
@@ -0,0 +1,22 @@
+import { afterAll, beforeAll, test } from 'vitest'
+import { app } from '../src/app'
+import request from 'supertest'
+
+beforeAll(async () => {
+  await app.ready()
+})
+
+afterAll(async () => {
+  await app.close()
+})
+
+test('user can create a new transaction', async () => {
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+    .expect(201)
+})
